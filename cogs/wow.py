90bc07ed2d955727670fc40f2ffa223943f5e4a3
Christoph Fischer <cfischer95@googlemail.com>
2018-05-18 11:08:58 +0200

    implemented bNetAPI

--- cogs/wow.py
+++ cogs/wow.py
@@ -1,6 +1,13 @@
 import discord
 from discord.ext import commands
 
+import os
+
+import json #bNet API
+import urllib.request
+
+WOW_API_TOKEN = open(os.path.join(os.path.dirname(__file__), "../tokens/bNetAPI")).read()
+
 class WoW:
     """Custom WoW Cog with basic commands"""
 
@@ -10,7 +17,16 @@ class WoW:
     @commands.command()
     async def wow(self):
         #Your code will go here
-        await self.bot.say("I can do stuff!")
+        await self.bot.say("WOW!")
+
+    @commands.command()
+    async def affixes(self):
+        await self.bot.say(WOW_API_TOKEN)
+        # Prints current affixes
+        url = urllib.request.urlopen("https://eu.api.battle.net/data/wow/mythic-challenge-mode/?namespace=dynamic-eu&locale=en_GB&access_token=" + WOW_API_TOKEN)
+        keystoneData = json.loads(url.read().decode())
+        await self.bot.say(keystoneData)
+
 
 def setup(bot):
     bot.add_cog(WoW(bot))
